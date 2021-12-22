ce8b10c46473e420563e06415ec80e2e26d3c783
FelipeColona <felipedev202@gmail.com>
2021-12-22 15:55:40 -0300

    first commit

---
+++ src/main/java/br/com/questionsbase/QuestionsBaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.questionsbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/questionsbase/QuestionsBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.questionsbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
